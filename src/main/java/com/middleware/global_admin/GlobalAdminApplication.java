2a2c6e7b1ec61922842209438c597aef60b2854b
MTL1014 <ravitejaganji49@gmail.com>
2025-04-04 14:33:12 +0530

    new

---
+++ src/main/java/com/middleware/global_admin/GlobalAdminApplication.java
@@ -0,0 +1,13 @@
+package com.middleware.global_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/middleware/global_admin/GlobalAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middleware.global_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
