3504aa72417890519dede95bdce3522d15c8ad33
MTL1025 <ravitejaganji49@gmail.com>
2025-04-09 13:09:41 +0530

    exceptions modified.

--- src/main/java/com/middleware/global_admin/Exceptions/OrganisationNotFoundException.java
+++ src/main/java/com/middleware/global_admin/Exceptions/OrganisationNotFoundException.java
@@ -1,6 +1,5 @@
 package com.middleware.global_admin.Exceptions;
 
-
 public class OrganisationNotFoundException extends RuntimeException {
     public OrganisationNotFoundException(String message) {
             super(message);
